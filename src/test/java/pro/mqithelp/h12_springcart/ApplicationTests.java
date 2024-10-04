21734aa131a783882f14112a211e664c6b33933e
mqWork <mqithelp@gmail.com>
2024-10-04 16:18:02 +0300

    init commit

---
+++ src/test/java/pro/mqithelp/h12_springcart/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.mqithelp.h12_springcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
